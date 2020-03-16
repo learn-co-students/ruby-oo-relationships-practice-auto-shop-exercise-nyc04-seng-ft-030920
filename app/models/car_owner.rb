dbae19fcf771979aff494b028d07f255cf8f43a5
replacem3nts <donny.landis@gmail.com>
2020-03-16 13:42:26 -0400

    updates files to meet all deliverables

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,28 @@
 class CarOwner
+attr_reader :name
 
-  attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def owner_cars
+    Car.all.select {|car| car.owner == self}
+  end
+
+  def mechanics
+    owner_cars.map {|car| car.mechanic}
+  end
+
+  def self.avg_cars_per_owner
+    num_cars_array = self.all.map {|owner| owner.owner_cars.length}
+    (num_cars_array.sum.to_f / num_cars_array.length.to_f).round(2)
+  end
 
 end
