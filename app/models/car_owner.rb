cc72525a2b131256be4d287e9babd3f4b2e55133
GaryZCLau <garyzclau@gmail.com>
2020-03-17 09:47:56 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,9 +1,37 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select {|car|
+    car.carowner == self
+  }
+  end
+
+  def mechanic
+    cars.map {|cars|
+    cars.mechanic
+  }
+  end
+
+  def averagecars
+    numofcars = Cars.all.length
+    numofowners = self.all.length
+    numofcars / numofowners
+    # self.all.length
+    # list of all sowners
+    # cars.length 
+    # list of all cars specific owner own
   end
 
 
